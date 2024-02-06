6631c13835ca941d532b2469ba1a950c6430feb4
ariannagiaccari <150803603+ariannagiaccari@users.noreply.github.com>
2024-02-06 19:20:32 +0100

    Add files via upload

--- dom/Manipolazione del DOM/index.js
+++ dom/Manipolazione del DOM/index.js
@@ -1,3 +1,26 @@
-const addProduct = () => {
-  // ...
-};
+// const addProduct = () => {
+//   const inputBox = document.getElementsByClassName("inputBox");
+//   const listContainer = document.querySelector("ul");
+
+//   const task = inputBox.value;
+
+//   if (task != "") {
+//     alert("type something!");
+//     return;
+//   } else {
+
+//     const li = document.createElement("li");
+//     li.innerHTML = `
+//       <label for="input">
+//         <input type="checkbox"></input>
+//         <span>${task}</span>
+//       </label>
+//       `;
+// listContainer.appendChild(li);
+// inputBox.value = "";
+//   };
+
+
+
+//   // ...
+// };
